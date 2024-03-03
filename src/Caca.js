75c43c282eb4ccd33548b7d14ea4c9fda094cdbf
mao_o <mao_o@live.com>
2024-03-03 03:28:59 -0600

    carrusel

---
+++ src/Caca.js
@@ -0,0 +1,10 @@
+var caja = [
+    "https://static.gniyonna.com/media/poster/900x600/100/5f71fbd900049a081c1f8a304fbcaa13.jpeg",
+    "https://static.gniyonna.com/media/poster/900x600/100/dec465f6f77555fd46303efbd5c357bd.jpg",
+    "https://static.gniyonna.com/media/poster/900x600/100/2b9aec156541501b8a76082d79dff269.jpg",
+    "https://static.gniyonna.com/media/poster/900x600/100/6348be264c7459c093eb072143240d8b.jpg",
+    "https://static.gniyonna.com/media/poster/900x600/100/9fe7b1c2350c7a5d63c20a9ae2effa05.jpeg",
+    "https://static.gniyonna.com/media/poster/900x600/100/0e4f76d09a598f73e41a7945738b5d77.jpg",
+    "https://static.gniyonna.com/media/poster/900x600/100/f6983901d1cd06a7debcb7e6f039f1b8.jpg",
+    "https://static.gniyonna.com/media/poster/900x600/100/cec52e9446224741f4d1fd3af9b96fb7.jpg"
+]
