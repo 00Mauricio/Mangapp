c8b521c855e8a5b041b1697b7be44022a1b8ea23
mao_o <mao_o@live.com>
2024-03-06 14:35:18 -0600

    se pudieron aplicar los estilos de tailwin al build

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const { iconsPlugin, getIconCollections } = require("@egoist/tailwindcss-icons")
+
+/** @type {import('tailwindcss').Config} */
+
+
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [    iconsPlugin({
+		// https://icones.js.org/
+		collections: getIconCollections(["heroicons","bx"]),
+	  }),],
+};
+
