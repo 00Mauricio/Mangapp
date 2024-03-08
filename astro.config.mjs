41fcfc23da8ba8637ee3f6af1810deb093a03a00
mao_o <mao_o@live.com>
2024-03-08 16:38:04 -0600

    configuracio de puerto 8000

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,14 @@ export default defineConfig({
   integrations: [
     tailwind( { nesting: true,configFile: './tailwind.config.js',})
   ],
+  site: 'https://manga-app-mau-proyects.koyeb.app',
   // esta parte esla necesaria para costruir el css en el build
   build: {
     inlineStylesheets: 'always'
-  }
+  },
+  
+    server: { port: 8000 },
+  
 
   
 });
