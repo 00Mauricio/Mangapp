0b57c55b0133a9a37aa6f4e7dcb9ce9277506f53
mao_o <mao_o@live.com>
2024-03-08 11:45:27 -0600

    correccion del built

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,13 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind( { nesting: true,})]
+  integrations: [
+    tailwind( { nesting: true,configFile: './tailwind.config.js',})
+  ],
+  // esta parte esla necesaria para costruir el css en el build
+  build: {
+    inlineStylesheets: 'always'
+  }
+
+  
 });
